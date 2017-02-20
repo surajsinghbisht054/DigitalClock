c06c7d5c87c39f5b38c46aee463be41116907ca3
Suraj Singh Bisht <surajsinghbisht054@gmail.com>
2017-02-20 15:22:06 +0530

    Latest Update

--- digitalclonck.py
+++ digitalclonck.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+# ---------------- READ ME ---------------------------------------------
+# This Script is Created Only For Practise And Educational Purpose Only
+# This Script Is Created For http://bitforestinfo.blogspot.com
+# This Script is Written By
+__author__='''
+
+######################################################
+                By S.S.B Group                          
+######################################################
+
+    Suraj Singh
+    Admin
+    S.S.B Group
+    surajsinghbisht054@gmail.com
+    http://bitforestinfo.blogspot.in/
+
+    Note: We Feel Proud To Be Indian
+######################################################
+'''
 try:
 	import Tkinter
 except:
